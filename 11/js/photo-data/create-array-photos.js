2153883989122abc7422d7d5b037d22289733e7f
Keks <keks@htmlacademy.ru>
2024-11-08 14:32:21 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/photo-data/create-array-photos.js
@@ -0,0 +1,20 @@
+import { getRandomInteger } from '../util.js';
+import { COUNTER,MAXNUMBEROFCOMMENTS, PHOTODESCRIPTION, Likes } from './photo-сonstants.js';
+import { createArrayComments } from './create-comments.js';
+
+const createArrayPhotos = (number) => (
+  [...new Array(number)].map((_, index) => (
+    {
+      id: `${index + 1}`,
+      url: `photos/${index + 1}.jpg`,
+      description: PHOTODESCRIPTION[getRandomInteger(1, index + 1)],
+      likes: getRandomInteger(Likes.MIN, Likes.MAX),
+      comments: createArrayComments(getRandomInteger(0, MAXNUMBEROFCOMMENTS), index + 1),
+    }
+  )
+  )
+);
+
+export const photos = createArrayPhotos(COUNTER);
+
+
---
+++ 11/js/thumbnail-rendering.js
@@ -0,0 +1,31 @@
+import {photos} from './photo-data/create-array-photos.js';
+import { open } from './preview-rendering.js';
+
+const windowWithThumbnails = document.querySelector('.pictures');
+
+const templateOfThumbnail = document.querySelector('#picture').content.querySelector('.picture');
+
+const fragmentForThumbnails = document.createDocumentFragment();
+
+const renderPhoto = (photo) => {
+  const newThumbnail = templateOfThumbnail.cloneNode(true);
+  newThumbnail.querySelector('.picture__img').src = photo.url;
+  newThumbnail.querySelector('.picture__img').alt = photo.description;
+  newThumbnail.querySelector('.picture__likes').textContent = photo.likes;
+  newThumbnail.querySelector('.picture__comments').textContent = photo.comments.length;
+  newThumbnail.addEventListener('click', () => {
+    open(photo);
+  });
+  return newThumbnail;
+};
+
+const renderPhotos = (arrayPhotos) => {
+  arrayPhotos.forEach((photo) => {
+    fragmentForThumbnails.appendChild(renderPhoto(photo));
+  }
+  );
+
+  windowWithThumbnails.appendChild(fragmentForThumbnails);
+};
+
+renderPhotos(photos);
