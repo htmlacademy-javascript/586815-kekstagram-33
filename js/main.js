03f1d1cdf71d6888b092fc197d5a58f1e9a686c0
Даниил Топ <cool.topolskiy.gmail.com>
2024-11-26 12:35:48 +0300

    filtres v3

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {renderPhotos } from './thumbnail-rendering.js';
+import { renderPhotos } from './thumbnail-rendering.js';
 import './preview-rendering.js';
 import './validation-upload-image.js';
 import './setup-upload-image.js';
