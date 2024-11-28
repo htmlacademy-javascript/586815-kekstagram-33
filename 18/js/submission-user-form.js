86cab21f77232ca59c11f9161e220589fcca75ca
Keks <keks@htmlacademy.ru>
2024-11-28 19:16:07 +0300

    :heavy_check_mark: Сборка #18

---
+++ 18/js/submission-user-form.js
@@ -0,0 +1,101 @@
+import { form, pristine, close } from './validation-upload-image.js';
+import { sendData } from './api.js';
+import { isEscapeKey } from './util.js';
+
+const submitButton = form.querySelector('.img-upload__submit');
+const templateSuccess = document.querySelector('#success').content.querySelector('.success');
+const templateError = document.querySelector('#error').content.querySelector('.error');
+
+const SubmitButtonText = {
+  IDLE: 'Опубликовать',
+  SENDING: 'Публикация...'
+};
+
+const blockSubmitButton = () => {
+  submitButton.disabled = true;
+  submitButton.textContent = SubmitButtonText.SENDING;
+};
+
+const unblockSubmitButton = () => {
+  submitButton.disabled = false;
+  submitButton.textContent = SubmitButtonText.IDLE;
+};
+
+// Удаляет переданный контейнер и снимает обработчики событий
+const setupCloseHandlers = (container, closeButton, inner, options = () => {}) => {
+  const removeContainer = () => {
+    container.remove();
+    document.removeEventListener('keydown', onEscapePress);
+    document.removeEventListener('click', onOutsideClick);
+    if (typeof options === 'function') {
+      options();
+    }
+  };
+
+  function onEscapePress (evt) {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      removeContainer();
+    }
+  }
+
+  function onOutsideClick (evt) {
+    if (!inner.contains(evt.target)) {
+      removeContainer();
+    }
+  }
+
+  closeButton.addEventListener('click', removeContainer);
+  document.addEventListener('keydown', onEscapePress);
+  document.addEventListener('click', onOutsideClick);
+};
+
+//настройка сообщения об успешной загрузке
+const showSuccess = () => {
+  const fragment = document.createDocumentFragment();
+  const successContainer = templateSuccess.cloneNode(true);
+  const innerContainer = successContainer.querySelector('.success__inner');
+  const successButton = successContainer.querySelector('.success__button');
+  fragment.appendChild(successContainer);
+  document.body.appendChild(fragment);
+
+  setupCloseHandlers(successContainer, successButton, innerContainer , close);
+};
+
+//настройка сообщения о неуспешной загрузке
+const showError = () => {
+  const fragment = document.createDocumentFragment();
+  const errorContainer = templateError.cloneNode(true);
+  const innerContainer = errorContainer.querySelector('.error__inner');
+  const errorButton = errorContainer.querySelector('.error__button');
+  fragment.appendChild(errorContainer);
+  document.body.appendChild(fragment);
+
+  setupCloseHandlers(errorContainer, errorButton, innerContainer);
+};
+
+const setUserFormSubmit = () => {
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    if (pristine.validate()) {
+      blockSubmitButton();
+      sendData(new FormData(evt.target))
+        .then(
+          (response) => {
+            if (!response.ok) {
+              showError();
+            }
+            showSuccess();
+          }
+        )
+        .catch(
+          () => {
+            showError();
+          }
+        )
+        .finally(unblockSubmitButton);
+    }
+  });
+};
+
+export { setUserFormSubmit };
---
+++ 18/js/validation-upload-image.js
@@ -0,0 +1,118 @@
+import { isEscapeKey } from './util.js';
+import './../vendor/pristine/pristine.min.js';
+import './setup-upload-image.js';
+import { resetEffectsSlider } from './setup-upload-image.js';
+
+const MAX_DESCRIPTION_LENGTH = 140;
+const MAX_AMOUNT_HASHTAGS = 5;
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
+
+const form = document.querySelector('.img-upload__form');
+const previewNewPhoto = document.querySelector('.img-upload__overlay');
+const buttonCancelPreviewUpload = document.querySelector('.img-upload__cancel');
+const descriptionField = form.querySelector('.text__description');
+const hashtagsField = form.querySelector('.text__hashtags');
+const previewUploadimage = previewNewPhoto.querySelector('img');
+const fileChooser = document.querySelector('.img-upload__input[type=file]');
+
+const initUploadImage = () => {
+  const file = fileChooser.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+  if (matches) {
+    previewUploadimage.src = URL.createObjectURL(file);
+  }
+};
+
+function validateDescriptionLength (value) {
+  return value.length <= MAX_DESCRIPTION_LENGTH;
+}
+
+function validateCombinedHashtags(value) {
+  const hashtags = value.trim().split(/\s+/);
+  const lowercaseHashtags = hashtags.map((hashtag) => hashtag.toLowerCase());
+
+  // Проверка на валидность каждого хэштега
+  const isValid = hashtags.every((hashtag) => {
+    if (hashtag === '') {
+      return true;
+    }
+    return /^#[a-zA-Zа-яА-Я0-9]{1,19}$/.test(hashtag);
+  });
+
+  if (!isValid) {
+    return 'Введён невалидный хэштег';
+  }
+
+  // Проверка на дубликаты
+  const hasDuplicates = new Set(lowercaseHashtags).size !== hashtags.length;
+  if (hasDuplicates) {
+    return 'Хэштеги повторяются';
+  }
+
+  //Проверка на количество хэштегов
+  const hasAmountHashtags = hashtags.length > MAX_AMOUNT_HASHTAGS;
+  if (hasAmountHashtags) {
+    return 'Превышено количество хэштегов';
+  }
+
+
+  // Всё прошло успешно
+  return true;
+}
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'div',
+  errorTextClass: 'img-upload__field-wrapper--error'
+});
+
+pristine.addValidator(
+  descriptionField,
+  validateDescriptionLength,
+  `Длина комментария больше ${ MAX_DESCRIPTION_LENGTH } символов`
+);
+
+pristine.addValidator(
+  hashtagsField,
+  (value) => validateCombinedHashtags(value) === true,
+  (value) => validateCombinedHashtags(value)
+);
+
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt) && !document.querySelector('.error')) {
+    evt.preventDefault();
+    close();
+  }
+};
+
+function close () {
+  document.body.classList.remove('modal-open');
+  previewNewPhoto.classList.add('hidden');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  form.reset();
+  resetEffectsSlider();
+  pristine.reset();
+}
+
+const open = () => {
+  document.body.classList.add('modal-open');
+  previewNewPhoto.classList.remove('hidden');
+  document.addEventListener('keydown', onDocumentKeydown);
+  initUploadImage();
+};
+
+form.addEventListener('change', open);
+
+buttonCancelPreviewUpload.addEventListener('click', close);
+
+const onTextFieldFocus = (evt) => {
+  evt.stopPropagation();
+};
+
+descriptionField.addEventListener('keydown', onTextFieldFocus);
+hashtagsField.addEventListener('keydown', onTextFieldFocus);
+
+export { form, pristine, close };
